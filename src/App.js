6b804e08d35a7f0b03ead36f1641b4e9cb91744a
primeiro commit <you@example.com>
2019-09-18 14:16:39 -0300

    v0.01

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Cards from 'react-credit-cards';
+import 'react-credit-cards/es/styles-compiled.css';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [numero, setNumero] = useState('');
+  const [nome, setNome] = useState('');
+  const [validade, setValidade] = useState('');
+  const [cvc, setCvc] = useState('');
+  const [focused, setFocused] = useState('');
+
+  const inFocused = (e) => {
+    setFocused(e.target.type);
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      
+      <h2>Sua compra está quase finalizada...</h2>
+
+        <form onSubmit={submitForm}>
+          <label>Preencha seus dados abaixo:</label>
+
+          <div className="area">
+            <Cards 
+              number={numero}
+              name={nome}
+              expiry={validade}
+              cvc={cvc}
+              focused={focused}
+            />
+          
+            <div className="numero-nome">
+              <input type="number" placeholder="Número do cartão" className="numero"
+                onChange={(event) => (setNumero(event.target.value))}
+                onFocus={inFocused}
+               />
+              <input type="text" placeholder="Nome completo" className="nome"
+                onChange={(event) => (setNome(event.target.value))}
+                onFocus={inFocused}
+              />
+              
+              <div className="validade-cvc">
+                <input type="number" placeholder="Validade" className="validade"
+                  onChange={(event) => (setValidade(event.target.value))}
+                  onFocus={inFocused}
+                />
+                <input type="number" placeholder="CVC" className="cvc"
+                  onChange={(event) => (setCvc(event.target.value))}
+                  onFocus={inFocused}
+                />
+              </div>
+
+              <button>PAGAR AGORA</button>
+            </div>
+          </div>  
+        </form>
+    
     </div>
   );
 }
-
-export default App;
